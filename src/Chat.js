a178dea44f7be722c71b1d245b28ca3c2f98f243
nik580 <580@users.noreply.github.com>
2020-09-29 15:08:23 +0100

    styling chat window

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from "react";
 import "./Chat.css";
 import { Avatar, IconButton} from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons";
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined} from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
     const [seed, setSeed] = useState("");
@@ -39,9 +41,20 @@ function Chat() {
 
         </div>
         <div className="chat__body">
-
+            <p className={'chat__message ${true && "chat__receiver"}'}>
+             <span className="chat__name">Nikhil Nandeshwar</span>   
+                Hi There !!! 
+             <span className="chat__timestamp"> 10:25pm</span>
+                </p>
+         
         </div>
         <div className="chat__footer">
+           <InsertEmoticonIcon />
+           <form>
+               <input placeholder="Type a message" type="text" />
+               <button>Send a message</button>
+               </form>
+            <MicIcon /> 
 
         </div>
 
